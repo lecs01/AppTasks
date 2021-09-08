3b68e01fbb2b029f2a4c51367dfde7c1ba000aab
Leandro Claer de Souza <lecs.01@outlook.com>
2021-09-08 18:23:19 -0300

    implementacao do binding e criacao da intent no MainActivity.kt; ajustes do fabButton;

--- app/src/main/java/br/com/lecs/apptasks/MainActivity.kt
+++ app/src/main/java/br/com/lecs/apptasks/MainActivity.kt
@@ -1,11 +1,26 @@
 package br.com.lecs.apptasks
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.lecs.apptasks.databinding.ActivityMainBinding
+import br.com.lecs.apptasks.ui.AddTaskActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+        insertListeners()
+    }
+
+    private fun insertListeners() {
+        binding.fab.setOnClickListener {
+            startActivity(Intent(this, AddTaskActivity::class.java))
+        }
     }
 }
