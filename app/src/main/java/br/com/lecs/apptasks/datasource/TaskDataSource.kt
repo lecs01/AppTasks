b871e0230ab2085ceca297ec4947ba1c8e8b219b
Leandro Claer de Souza <lecs.01@outlook.com>
2021-09-09 00:50:39 -0300

    app versao 1.2

--- app/src/main/java/br/com/lecs/apptasks/datasource/TaskDataSource.kt
+++ app/src/main/java/br/com/lecs/apptasks/datasource/TaskDataSource.kt
@@ -14,7 +14,6 @@ object TaskDataSource {
             list.remove(task)
             list.add(task)
         }
-        list.add(task.copy(id = list.size + 1))
     }
 
     fun findById(taskId: Int) = list.find { it.id == taskId }
